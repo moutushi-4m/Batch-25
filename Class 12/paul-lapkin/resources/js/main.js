c49837417298462eea8f035b8549a57062a43078
moutushi-4m <sharmin.moutushi94@gmail.com>
2023-01-01 19:45:35 +0600

    first commit

---
+++ Class 12/paul-lapkin/resources/js/main.js
@@ -0,0 +1,14 @@
+//MIX IT UP
+var mixer = mixitup('.work-grid');
+
+//WOW 
+
+new WOW().init();
+
+// MENU
+function openMenu() {
+    document.getElementById('menu-container').style.width = "100%";
+}
+function closeMenu() {
+    document.getElementById('menu-container').style.width = "0%";
+}
